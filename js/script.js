8f97b53122a12cbcd59ac9b3b8a93e36746f4662
Timothy-Manwell <113055850+Timothy-Manwell@users.noreply.github.com>
2022-10-24 20:17:38 -0400

    js and css

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit2-04-PWA/sw.js", {
+    scope: "/ICS2O-Unit2-04-PWA/",
   })
 }
 
